589396fad7f9491f867fc154bdb928cca481e9c5
m1994d <marlondiaz2994@gmail.com>
2022-08-16 12:56:58 -0500

    functions

--- functions_shell.c
+++ functions_shell.c
@@ -1,10 +1,11 @@
 #include "shell.h"
 
 /**
- * _strlen - returns the length of a string.
- * @s: Pointer to string to measure
- * Return: Lenght of the string
+ * _strlen - this returns the length of a string.
+ * @s: a Pointer to the string to measure.
+ * Return: to the length of the string.
  */
+
 int _strlen(char *s)
 {
 	int i;
@@ -23,11 +24,11 @@ int _strlen(char *s)
 }
 
 /**
- * _strcpy - copies the string pointed to by src, including the terminating
+ * _strcpy - this copies the string pointed to by src, including the null byte (\0)
  * null byte (\0), to the buffer pointed to by dest.
- * @dest: Pointer to destiny
- * @src: Pointer to source
- * Return: pointer to dest
+ * @dest: a pointer to the destination.
+ * @src: a pointer to the origin.
+ * Return: Pointer to dest
  */
 
 char *_strcpy(char *dest, char *src)
@@ -39,4 +40,76 @@ char *_strcpy(char *dest, char *src)
 	for (i = 0; i <= len; i++)
 		*(dest + i) = *(src + i);
 	return (dest);
+}
+
+/**
+ * _strcat - this concatenates two strings.
+ * @dest: a pointer to the destination string.
+ * @src: a Pointer to the source string
+ * Return: a Pointer to the resulting string dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int lendest;
+	int lensrc;
+	int lenfdest;
+	int i;
+
+	lendest = _strlen(dest);
+	lensrc = _strlen(src);
+	lenfdest = lendest + lensrc;
+	i = 0;
+	while (lendest <= lenfdest)
+	{
+		*(dest + lendest) = *(src + i);
+		lendest++;
+		i++;
+	}
+	return (dest);
+}
+
+/**
+ * _strcmp - this compares 2 strings
+ * @s1: a Pointer to the first string
+ * @s2: a Pointer to the second string
+ * Return: to the difference between the first 2 different ASCII characters
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+	int ret;
+
+	ret = 0;
+	for (i = 0 ; *(s1 + i) != '\0' ; i++)
+	{
+		if (*(s1 + i) != *(s2 + i))
+		{
+			ret = *(s1 + i) - *(s2 + i);
+			break;
+		}
+	}
+	return (ret);
+}
+
+/**
+ * count_words - Count the words in each string, and the words are separated by spaces.
+ * @s: is a string to count words.
+ * Return: Number of words
+ */
+
+int count_words(char *s)
+{
+	int ret, i;
+
+	ret = 0;
+	if (s == NULL)
+		return (ret);
+	for (i = 0; s[i]; i++)
+	{
+		if (s[i] == ' ' || s[i] == '	')
+			ret++;
+	}
+	return (ret);
 }
