97945b5e17816e841e7fd390c05ca2911baffd9c
NolramGit <nolrammado@gmail.com>
2022-08-17 17:15:29 -0500

    upload files for testing tasks

---
+++ builtin.c
@@ -0,0 +1,37 @@
+#include "my_shell.h"
+/**
+ * built_in - builtin functions of shell
+ * @sll: shell structure
+ * @envs: environements
+ * Return: int
+ */
+int built_in(sll_t *sll, char **envs)
+{
+	look_char(sll);
+
+	if (_strcmp(sll->Command, "exit") == 0)
+	{
+		ALLfree(sll);
+		exit(EXIT_SUCCESS);
+	}
+
+	if (_strcmp(sll->Command, "env") == 0)
+	{
+		while (*envs)
+		{
+			printf("%s\n", *envs);
+			envs++;
+		}
+		ALLfree(sll);
+		return (1);
+	}
+
+	if (_strcmp(sll->Command, "cd") == 0)
+	{
+		printf("I'm cd Command\n");
+		ALLfree(sll);
+		return (1);
+	}
+
+	return (0);
+}
--- main.c
+++ main.c
@@ -1,19 +1,49 @@
-#include "shell.h"
-
+#include "my_shell.h"
 /**
- * main - Main function for shell
- * @argc: Argument counter
- * @argv: Argument vector
- * Return: Always 0
+ * main - build a shell
+ * @argc: number of arguments
+ * @argv: pointer to the address of arguments
+ * @envs: environment
+ * Return: int
  */
-
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc __attribute__((unused)),
+char *argv[] __attribute__((unused)), char **envs)
 {
-	char *prompt;
-	size_t aux;
+	char *line = NULL;
+	int count = 0;
+	char *CommandPath = NULL, *envPath;
+	sll_t *sll;
 
-	prompt = "[$]:";
-	aux = 1;
-	init_shell(prompt, argv[0], aux);
-	return (0);
+	envPath = getenv("PATH");
+	signal(SIGINT, new_signal_handler); /*SIGINT = Interrupt the process*/
+
+	while (true)
+	{
+		line = NULL;
+		CommandPath = NULL;
+		count++;
+
+		if (isatty(STDIN_FILENO) == 1)
+		{
+			if (write(STDOUT_FILENO, "$ ", 2) == EOF) /* print the prompt*/
+				exit(EXIT_FAILURE);
+		}
+		GETline(&line); /* Line provided by user*/
+		sll = parse_sll(line);
+		free(line);
+
+		if (built_in(sll, envs))
+			continue;
+
+		CommandPath = getpath_sll(sll, envPath);
+		if (!CommandPath)
+		{
+			printf("%s: %d: %s: not found\n", argv[0], count, sll->Command);
+			free(CommandPath), ALLfree(sll);
+			continue;
+		}
+		nobuilt_in(sll, CommandPath);
+		free(CommandPath), ALLfree(sll);
+	}
+	return (EXIT_SUCCESS);
 }
